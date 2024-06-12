60eeeb15ae7dd47243bc33b055532af91aabd309
pedromax23 <pedromorgade1234@gmail.com>
2024-06-12 13:23:51 -0300

    First commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Navbar />
+        <main className="container mx-auto h-[calc(100vh-7rem)] flex justify-center items-center">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Users from "@/components/Users";
+
+async function fetchUsers() {
+  const res = await fetch('https://reqres.in/api/users');
+  const data = await res.json();
+  return data.data
+}
+
+export default async function Home() {
+
+  const users = await fetchUsers();
+
+  return (
+    <Users users={users}/>
+  );
+}
